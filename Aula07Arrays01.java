fe4c30a776d4f19aa4b7f8a44b54530af76b92eb
lucasvargass <lucas.vargas.pina25@gmail.com>
2025-10-23 20:07:19 -0300

    Arrays - Parte 01

--- Aula07Arrays01.java
+++ Aula07Arrays01.java
@@ -7,9 +7,11 @@ public class Aula07Arrays01 {
         // ARRAYS
 
         int[] vida = new int[3];
+
         vida[0] = 90;
         vida[1] = 80;
         vida[2] = 70;
+
         System.out.println(vida[0]);
         System.out.println(vida[1]);
         System.out.println(vida[2]);
